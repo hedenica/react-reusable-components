42b3f74292122e1b53bdde4327d6954d849a97fc
hedenica <hedenica@hotmail.com>
2020-06-25 18:51:19 -0300

    ✨  changing to class component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Table from './components/Table';
 import './App.css';
 
-function App() {
-  const autores = [
-    {
-      nome: 'Paulo',
-      livro: 'React',
-      preco: '1000'
-    },
-    {
-      nome: 'Daniel',
-      livro: 'Java',
-      preco: '99'
-    },
-    {
-      nome: 'Marcos',
-      livro: 'Design',
-      preco: '150'
-    },
-    {
-      nome: 'Bruno',
-      livro: 'DevOps',
-      preco: '100'
-    },
-  ];
+class App extends Component {
 
-  return (
-    <div className="App">
-      <Table autores={autores} />
-    </div>
-  );
+  state = {
+    autores: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      },
+    ],
+  }
+
+  render (){
+    return (
+      <div className="App">
+        <Table autores={this.state.autores} />
+      </div>
+    );
+  }
 }
 
 export default App;
